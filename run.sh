350edf6cc8b63fc6d81b257998d31b43cbdf06b8
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-01-27 05:37:55 +0300

    Use host network stack

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 docker run --name 1c-server \
-  --hostname `hostname` \
+  --net host \
   --detach \
-  --publish 1540-1541:1540-1541 \
-  --publish 1560-1591:1560-1591 \
   --volume 1c-server-home:/home/usr1cv8 \
   --volume 1c-server-logs:/var/log/1C \
   --volume /etc/localtime:/etc/localtime:ro \
